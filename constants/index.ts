8a4fe83c2c2d41e4b0cb70d0873e27c2699e3ded
Joshua Emorwodia <joshuaemorwodia@gmail.com>
2025-06-25 09:21:05 +0100

    Add interfaces, constants, and image assets

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+// constants/index.ts
+import { Property } from '@/interfaces';
+
+export const PROPERTY_LISTINGS: Property[] = [
+  {
+    id: 1,
+    title: 'Modern Apartment in Lekki',
+    location: 'Lekki, Lagos',
+    price: '₦300,000/month',
+    image: '/images/property1.jpg',
+  },
+  {
+    id: 2,
+    title: 'Cozy Studio in Yaba',
+    location: 'Yaba, Lagos',
+    price: '₦200,000/month',
+    image: '/images/property2.jpg',
+  },
+  {
+    id: 3,
+    title: 'Spacious Duplex in Abuja',
+    location: 'Gwarinpa, Abuja',
+    price: '₦500,000/month',
+    image: '/images/property3.jpg',
+  },
+];
+
---
+++ interfaces/index.ts
@@ -0,0 +1,8 @@
+// interfaces/index.ts
+export interface Property {
+  id: number;
+  title: string;
+  location: string;
+  price: string;
+  image: string;
+}
